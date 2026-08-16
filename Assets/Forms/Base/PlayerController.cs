fbf5d028c84bd02079628504a23c604d8ba51771
Mint-Hans <2718663285@qq.com>
2026-08-16 20:55:54 +0800

    feature:增加接口

--- Assets/Forms/Base/PlayerController.cs
+++ Assets/Forms/Base/PlayerController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AbilitySystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -109,6 +110,29 @@ public class PlayerController : MonoBehaviour
     {
         if (_formTypeToIndex.TryGetValue(formType, out int index))
             SwitchToForm(index);
+    }
+
+    public FormType GetCurrentForm() =>
+        ActiveForm != null ? ActiveForm.FormType : FormType.Slime;
+
+    public List<FormType> GetUnlockedForms() => new List<FormType>(_unlockedForms);
+
+    public void RestoreForms(FormType current, List<FormType> unlocked)
+    {
+        _unlockedForms.Clear();
+        _unlockedForms.Add(FormType.Slime);
+        if (unlocked != null)
+            foreach (var f in unlocked)
+                _unlockedForms.Add(f);
+        _unlockedForms.Add(current);
+        SwitchToFormByType(current);
+    }
+
+    public BaseForm GetForm(FormType type)
+    {
+        if (_formTypeToIndex.TryGetValue(type, out int index) && allForms != null && index < allForms.Length)
+            return allForms[index];
+        return null;
     }
 
     private void Update()
