37e592edc3ff3bdfd9c4ff1822d082fc829e7d04
OyingyO <a2358043671@163.com>
2026-08-20 16:58:54 +0800

    feat:更新1-3地图，新增转轴

---
+++ Assets/Maps/Interactables/Scripts/Axle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Axle : MonoBehaviour
+{
+    [Header("旋转速度")]
+    [Tooltip("每秒旋转的角度，正值为逆时针")]
+    public float rotationSpeed = 30f; // 可调
+
+    void FixedUpdate() // 使用FixedUpdate处理物理相关
+    {
+        // 绕Z轴旋转，实现2D平面内的旋转
+        transform.Rotate(0f, 0f, rotationSpeed * Time.fixedDeltaTime);
+    }
+}
